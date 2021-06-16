785f9877b75f34e6877ec98e1683f913a63908dc
nottak0409 <nottak0409@gmail.com>
2021-06-17 07:54:07 +0900

    error change

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use LINEBot\HTTPClient\CurlHTTPClient;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
