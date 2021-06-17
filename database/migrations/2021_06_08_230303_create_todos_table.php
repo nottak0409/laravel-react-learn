9ba4783c38cecbca2ea6215323abce6af264b567
nottak0409 <nottak0409@gmail.com>
2021-06-18 07:39:22 +0900

    change migrationfile

--- database/migrations/2021_06_08_230303_create_todos_table.php
+++ database/migrations/2021_06_08_230303_create_todos_table.php
@@ -15,6 +15,7 @@ class CreateTodosTable extends Migration
     {
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id')->unsigned();
             $table->string('title');
             $table->timestamps();
         });
