23342f043765f0f7fbb6a0559a202534b40716ef
nottak0409 <nottak0409@gmail.com>
2021-06-21 10:01:47 +0900

    style change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,7 +23,7 @@
         </style>
     </head>
     <body class="antialiased">
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+        <div class="relative flex justify-center min-h-screen bg-gray-100 dark:bg-gray-900 py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
@@ -42,7 +42,7 @@
                 </div>
             @endif
             <div class="container">
-                <h3 class="mt-5">Todo 管理アプリ</h3>
+                <h3 class="mt-10">Todo 管理アプリ</h3>
                 <div id="example"></div>
             </div>
         </div>
