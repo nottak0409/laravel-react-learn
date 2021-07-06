7779ef474051961313292a46ead79b944c8eddd2
nottak0409 <nottak0409@gmail.com>
2021-07-06 20:57:06 +0900

    redux追加

---
+++ resources/js/actions/index.js
@@ -0,0 +1,22 @@
+import { CREATE_MEMOS, EDIT_MEMOS, DELETE_MEMOS } from './types';
+
+export const CREATE_MEMOS = ({ title, url }) => ({
+    type: CREATE_MEMOS,
+    payload: {
+
+    }
+});
+
+export const EDIT_MEMOS = ({ title, url }) => ({
+    type: EDIT_MEMOS,
+    payload: {
+
+    }
+});
+
+export const DELETE_MEMOS = ({ title, url }) => ({
+    type: DELETE_MEMOS,
+    payload: {
+
+    }
+});
