e9a56c05bf15d62868dca8e7450453950a7a32ff
nottak0409 <nottak0409@gmail.com>
2021-07-07 19:01:43 +0900

    redux

--- resources/js/actions/index.js
+++ resources/js/actions/index.js
@@ -1,20 +1,20 @@
 import { CREATE_MEMOS, EDIT_MEMOS, DELETE_MEMOS } from './types';
 
-export const createMemos = ({ title, url }) => ({
+export const createMemos = ({ title, content }) => ({
     type: CREATE_MEMOS,
     payload: {
 
     }
 });
 
-export const editMemos = ({ title, url }) => ({
+export const editMemos = ({ title, content }) => ({
     type: EDIT_MEMOS,
     payload: {
 
     }
 });
 
-export const deleteMemos = ({ title, url }) => ({
+export const deleteMemos = id => ({
     type: DELETE_MEMOS,
     payload: {
 
