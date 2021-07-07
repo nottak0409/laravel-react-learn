6b47bfd18369dd3d42ed0f4d8ca1e466430b2bb3
nottak0409 <nottak0409@gmail.com>
2021-07-07 18:56:30 +0900

    reducer修正

--- resources/js/actions/index.js
+++ resources/js/actions/index.js
@@ -1,20 +1,20 @@
 import { CREATE_MEMOS, EDIT_MEMOS, DELETE_MEMOS } from './types';
 
-export const CREATE_MEMOS = ({ title, url }) => ({
+export const createMemos = ({ title, url }) => ({
     type: CREATE_MEMOS,
     payload: {
 
     }
 });
 
-export const EDIT_MEMOS = ({ title, url }) => ({
+export const editMemos = ({ title, url }) => ({
     type: EDIT_MEMOS,
     payload: {
 
     }
 });
 
-export const DELETE_MEMOS = ({ title, url }) => ({
+export const deleteMemos = ({ title, url }) => ({
     type: DELETE_MEMOS,
     payload: {
 
