c2da291db1966de1c47ff3a5cfdc939f2961866d
nottak0409 <nottak0409@gmail.com>
2021-07-24 10:40:37 +0900

    package 追加

--- resources/js/stores/store.js
+++ resources/js/stores/store.js
@@ -1,8 +1,10 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import events from '../reducer/index.js';
 
-const state_value = {
-    todos: [],
-};
+const composedEnhancer = composeWithDevTools(
+    applyMiddleware(thunkMiddleware)
+)
 
-export default createStore(events);
+export default createStore(events, composedEnhancer);
